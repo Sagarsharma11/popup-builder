b3603ca12d73efe94c121db5bf9d98c0110c1a8e
Sagar Sharma <sagar@hellomentor.in>
2025-11-25 20:41:23 +0530

    some

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Client>{children}</Client>
+      {children}
       </body>
     </html>
   );
