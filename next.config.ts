20c932e0d2d1e481b25c57a0a996cbd19f9e1f89
Sagar Sharma <sagar@hellomentor.in>
2025-11-25 20:43:16 +0530

    some

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
+// @ts-nocheck
+/* eslint-disable */
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
